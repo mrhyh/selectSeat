ceffe04b3f6148fd030b1acb54a22335353f10c2
mrhyh <mrhyh@qq.com>
2016-08-16 13:00:15 +0800

    update

--- HSMScrollView/SMScrollView.m
+++ HSMScrollView/SMScrollView.m
@@ -18,8 +18,6 @@
 @property (weak, nonatomic) id scrollViewDelegate;  //用于存储delegate，防止野指针
 @property (assign, nonatomic) BOOL isFirstLogin;
 
-//临时添加
-@property (strong, nonatomic) KyoRowIndexView *rowIndexView;
 
 @end
 
@@ -263,6 +261,3 @@
 }
 
 @end
-// 版权属于原作者
-// http://code4app.com (cn) http://code4app.net (en)
-// 发布代码于最专业的源码分享网站: Code4App.com
